2e7f14896bc5af159c20d5c24fa75914a8ea1af3
Peter Jang <peter.x.jang@gmail.com>
2016-12-08 20:37:40 -0600

    Add employee index action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,10 @@
 class EmployeesController < ApplicationController
+  def index
+    # @employees = Employee.all
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees").body
+    render 'index.html.erb'
+  end
+
   def show
     # @employee = Employee.find_by(id: params[:id])
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}").body
